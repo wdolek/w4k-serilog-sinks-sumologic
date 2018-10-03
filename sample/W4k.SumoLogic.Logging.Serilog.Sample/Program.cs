fc7d768ad899f88f4c89cefd50cd82730dbbbc9e
Zdenek Havlin <wdolek@hotmail.com>
2018-10-03 18:21:06 +0200

    Add sample console application

---
+++ sample/W4k.SumoLogic.Logging.Serilog.Sample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+using W4k.SumoLogic.Logging.Serilog.Extensions;
+
+namespace W4k.SumoLogic.Logging.Serilog.Sample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // init logger programatically
+            var url = new Uri("http://localhost");
+            var sourceName = "w4k-serilog-sumologic";
+            Logger logFromFluent = new LoggerConfiguration()
+                .WriteTo.SumoLogic(url, sourceName: sourceName)
+                .CreateLogger();
+
+            logFromFluent.Information("Event: Initialized logger");
+
+            // init logger from configuration
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Logger logFromConfig = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            logFromConfig.Information("Event: Configured logger");
+
+            // we are done!
+            Console.ReadLine();
+        }
+    }
+}
